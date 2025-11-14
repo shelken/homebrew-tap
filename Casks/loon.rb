7ed269ca0020139bca897152aaa7cc5e0b839a98
shelken <shelken.pxk@gmail.com>
2025-11-15 01:54:31 +0800

    fix loon version

--- Casks/loon.rb
+++ Casks/loon.rb
@@ -1,11 +1,11 @@
 cask "loon" do
-  version "0.1(29)"
+  version "0.1.29"
   sha256 "31eee8a2a12059c0ddc8319b51ccd1c2ed668d6b09c662fb3199bb5cb809ac54"
 
   # 从版本号中提取 alpha 版本号
-  alpha_version = version[/\((\d+)\)/, 1]
+  alpha_version = version.split(".").last
 
-  url "https://github.com/Loon0x00/Loon4Mac/releases/download/#{version}/Loon_alpha_#{alpha_version}.dmg"
+  url "https://github.com/Loon0x00/Loon4Mac/releases/download/0.1(#{alpha_version})/Loon_alpha_#{alpha_version}.dmg"
   name "Loon"
   desc "Network tool"
   homepage "https://github.com/Loon0x00/Loon4Mac"
